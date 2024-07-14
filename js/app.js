917e4efd64c313e9cbd8c475c7594b195c2858aa
aallen417 <austin.allen417@gmail.com>
2024-07-14 15:03:47 -0400

    Fresh slate step 1 complete

--- js/app.js
+++ js/app.js
@@ -4,23 +4,18 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board = ["","","","","","","","",""]
-let turn = "X"
-let winner = false
-let tie = false
+let board
+let turn
+let winner
+let tie
 
 /*------------------------ Cached Element References ------------------------*/
 
-const squareEls = document.querySelector(".sqr")
-const messageEl = document.querySelector("#message")
+
 
 /*-------------------------------- Functions --------------------------------*/
 
-const init = () => {
-    render()
-}
 
-init()
 
 /*----------------------------- Event Listeners -----------------------------*/
 
