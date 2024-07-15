540b721e29249b1207b646c8495cb9c261276cd0
aallen417 <austin.allen417@gmail.com>
2024-07-15 16:09:00 -0400

    complete functioning ttt app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -9,28 +18,107 @@ let turn
 let winner
 let tie
 
-/*------------------------ Cached Element References ------------------------*/
-
+/*------- Cached Element References ------*/
 const squareEls = document.querySelectorAll(".sqr")
-messageEl = document.getElementById("message")
+const messageEl = document.getElementById("message")
+const resetBtnEl = document.getElementById("reset")
 
 
-/*-------------------------------- Functions --------------------------------*/
+/*-------- Functions -----------------*/
+init()
 
-const init = () => {
-    board = ["","","","","","","","",""]
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""]
+  turn = "X"
+  winner = false
+  tie = false
+  render()
+}
+
+function render() {
+  updateBoard()
+  updateMessage()
+}
+
+function updateBoard() {
+  board.forEach((cell, idx) => {
+    if (cell === "X") {
+      squareEls[idx].textContent = "X"
+      squareEls[idx].style.backgroundColor = "purple"
+    } else if (cell === "O") {
+      squareEls[idx].textContent = "O"
+      squareEls[idx].style.backgroundColor = "orange"
+    } else {
+      squareEls[idx].textContent = ""
+      squareEls[idx].style.backgroundColor = "white"
+    }
+  })
+}
+
+function updateMessage() {
+  if (!winner && !tie) {
+    messageEl.textContent = `It is ${turn}'s turn`
+  } else if (!winner && tie) {
+    messageEl.textContent = "Cat's game!!"
+  } else {
+    messageEl.textContent = `${turn} wins the game!`
+  }
+}
+
+function handleClick(evt) {
+  const squareIndex = parseInt(evt.target.id)
+  if (board[squareIndex] === "X" || board[squareIndex] === "O" || winner) {
+    return
+  }
+  placePiece(squareIndex)
+  checkForWinner()
+  checkForTie()
+  switchPlayerTurn()
+  render()
+}
+
+function placePiece(index) {
+  board[index] = turn
+}
+
+function checkForWinner() {
+  if (
+    (board[0] !== "" && board[0] === board[1] && board[0] === board[2]) ||
+    (board[3] !== "" && board[3] === board[4] && board[3] === board[5]) ||
+    (board[6] !== "" && board[6] === board[7] && board[6] === board[8]) ||
+    (board[0] !== "" && board[0] === board[3] && board[0] === board[6]) ||
+    (board[1] !== "" && board[1] === board[4] && board[1] === board[7]) ||
+    (board[2] !== "" && board[2] === board[5] && board[2] === board[8]) ||
+    (board[0] !== "" && board[0] === board[4] && board[0] === board[8]) ||
+    (board[2] !== "" && board[2] === board[4] && board[2] === board[6])
+  ) {
+    winner = true
+  }
+}
+
+function checkForTie() {
+  if (winner) {
+    return
+  }
+  if (!board.includes("")) {
+    tie = true
+  }
+}
+
+function switchPlayerTurn() {
+  if (winner) {
+    return
+  }
+  if (turn === "X") {
+    turn = "O"
+  } else {
     turn = "X"
-    winner = false
-    tie = false
-    render()
+  }
 }
 
-const render = () => {
+/*----------- Event Listeners ----------*/
+squareEls.forEach((squareEl) => {
+  squareEl.addEventListener("click", handleClick)
+})
 
-
-}
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
+resetBtnEl.addEventListener("click", init)
